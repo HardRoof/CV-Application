e995d135c95d744c1a9662a241e9690fb1dd7960
HardRoof <92646158+HardRoof@users.noreply.github.com>
2024-11-29 22:14:30 +0000

    Refactored

--- cv-application/src/components/PreviewSide.jsx
+++ cv-application/src/components/PreviewSide.jsx
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
 
 function PreviewSide(info, background, experience) {
-  const hasEducation = background?.title || background?.institution || background?.date;
-  const hasExperience = experience?.role || experience?.company || experience?.date ||
-                       experience?.city || experience?.phone || experience?.accomplishments
+  const hasEducation =
+    background?.title || background?.institution || background?.date;
+  const hasExperience =
+    experience?.role ||
+    experience?.company ||
+    experience?.date ||
+    experience?.city ||
+    experience?.phone ||
+    experience?.accomplishments;
   return (
     <>
       <section>
-        <h1 style={{fontWeight: 'bold'}}>{info.userName}</h1>
+        <h1 style={{ fontWeight: "bold" }}>{info.userName}</h1>
         <div>
-          <h3>{info.userEmail}</h3>
           <h3>{info.userEmail}</h3>
         </div>
       </section>
-      <section>{hasEducation && (
-        <h2>EDUCATION</h2>
-        <p>{background.title}</p>
-        <p>{background.date}</p>
-        <p>{background.institution}</p>
-      )}
+      <section>
+        {hasEducation && (
+          <>
+            <h2>EDUCATION</h2>
+            <p>{background.title}</p>
+            <p>{background.date}</p>
+            <p>{background.institution}</p>
+          </>
+        )}
       </section>
-      <section> {hasExperience && (
-        <h2>WORK EXPERIENCE</h2>
-        <p>{experience.role}</p>
-        <p>{experience.company}</p>
-        <p>{experience.date}</p>
-        <p>{experience.city}</p>
-        <p>{experience.phone}</p>
-        <p>{experience.accomplishments}</p>
-      )}
+      <section>
+        {hasExperience && (
+          <>
+            <h2>WORK EXPERIENCE</h2>
+            <p>{experience.role}</p>
+            <p>{experience.company}</p>
+            <p>{experience.date}</p>
+            <p>{experience.city}</p>
+            <p>{experience.phone}</p>
+            <p>{experience.accomplishments}</p>
+          </>
+        )}
       </section>
     </>
   );
