0fdd2baffd58fd5c7e9e5c4b45582d885f0ddf8d
HardRoof <92646158+HardRoof@users.noreply.github.com>
2024-11-29 22:15:36 +0000

    Created FormSide

--- cv-application/src/components/FormSide.jsx
+++ cv-application/src/components/FormSide.jsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
+import GeneralInfo from "./components/GeneralInfo";
+import AcademicBackground from "./components/AcademicBackground";
+import JobExperience from "./components/JobExperience";
 
-export default function FormSide() {
-  return <></>;
+
+export default function FormSide({
+  info,
+  background,
+  experience,
+  handleInfoChange,
+  handleBackgroundChange,
+  handleExperienceChange,
+}) {
+  return (
+    <div>
+    
+    </div>)
 }
