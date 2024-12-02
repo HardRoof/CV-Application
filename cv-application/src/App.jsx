5629b7321a76985fbfa24446575fea458c4dd32e
HardRoof <92646158+HardRoof@users.noreply.github.com>
2024-12-02 19:25:01 +0000

    Added footer header, and FormSide

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 // Alternatively: import React from "react"; useState is an object of react React.useState(initialValue);
-import FormSide from "./components/FormSide"
+import FormSide from "./components/FormSide";
 import PreviewSide from "./components/PreviewSide";
 import "./index.css";
 
 function App() {
   const [info, setInfo] = useState({
-		userName: "",   
-		userEmail: "",
-		userPhone: "",
-	});
+    userName: "",
+    userEmail: "",
+    userPhone: "",
+  });
 
   const [background, setBackground] = useState({
     title: "",
@@ -26,13 +26,13 @@ function App() {
     accomplishments: "",
   });
 
-	const handleInfoChange = (e) => {
-		const {name, value} = e.target;
-		setInfo({...info, [name]: value});
-	};
+  const handleInfoChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
 
-  const handleBackgroundChange = () => {
-    const [name, value] = e.target;
+  const handleBackgroundChange = (e) => {
+    const { name, value } = e.target;
     setBackground({ ...background, [name]: value });
   };
 
@@ -43,12 +43,41 @@ function App() {
 
   return (
     <>
-      <header>CV App</header>
+      <header>
+        <div className="header-content">
+          <h1>CV App</h1>
+          <div className="header-buttons">
+            <button>Font 1</button>
+            <button>Font 2</button>
+            <button>Font 3</button>
+          </div>
+        </div>
+      </header>
       <main>
-        <FormSide />
-        <PreviewSide info={info} background={background} experience={experience} />
+        <FormSide
+          info={info}
+          background={background}
+          experience={experience}
+          handleInfoChange={handleInfoChange}
+          handleBackgroundChange={handleBackgroundChange}
+          handleExperienceChange={handleExperienceChange}
+        />
+        <PreviewSide
+          info={info}
+          background={background}
+          experience={experience}
+        />
       </main>
-      <footer>CV App "Github logo"</footer>
+      <footer>
+        {" "}
+        CV App
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/900px-Octicons-mark-github.svg.png?20180806170715"
+          alt="Github"
+          id="github"
+          onClick={() => window.open("https://github.com/HardRoof", "_blank")}
+        />
+      </footer>
     </>
   );
 }
