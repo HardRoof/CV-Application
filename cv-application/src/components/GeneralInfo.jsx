0cddda404d08f8b69c7f83e91b8edf42466ea5ef
HardRoof <92646158+HardRoof@users.noreply.github.com>
2024-12-11 23:02:28 +0000

    Added labels to inputs

--- cv-application/src/components/GeneralInfo.jsx
+++ cv-application/src/components/GeneralInfo.jsx
@@ -1,26 +1,29 @@
 const GeneralInfo = ({ info, handleInfoChange }) => {
-	return (
-		<form>
-			<input
-				type="text"
-				name="userName"
-				value={info.userName}
-				onChange={handleInfoChange}
-			/>
-			<input
-				type="email"
-				name="userEmail"
-				value={info.userEmail}
-				onChange={handleInfoChange}
-			/>
-			<input
-				type="tel"
-				name="userPhone"
-				value={info.userPhone}
-				onChange={handleInfoChange}
-			/>
-		</form>
-	)
-}
+  return (
+    <form>
+      <label htmlFor="userName">Full Name</label>
+      <input
+        type="text"
+        name="userName"
+        value={info.userName}
+        onChange={handleInfoChange}
+      />
+      <label htmlFor="userEmail">Email</label>
+      <input
+        type="email"
+        name="userEmail"
+        value={info.userEmail}
+        onChange={handleInfoChange}
+      />
+      <label htmlFor="userPhone">Phone number</label>
+      <input
+        type="tel"
+        name="userPhone"
+        value={info.userPhone}
+        onChange={handleInfoChange}
+      />
+    </form>
+  );
+};
 
 export default GeneralInfo;
