806a58c3ec0c78cfafd2e3da1ba455429ed8709d
HardRoof <92646158+HardRoof@users.noreply.github.com>
2024-12-12 23:49:46 +0000

    Added labels and updated new date field

--- cv-application/src/components/AcademicBackground.jsx
+++ cv-application/src/components/AcademicBackground.jsx
@@ -1,22 +1,32 @@
-function AcademicBackground({background, handleBackgroundChange}) {
+function AcademicBackground({ background, handleBackgroundChange }) {
   return (
     <form>
+      <label htmlFor="title">Degree</label>
       <input
         type="text"
         name="title"
         value={background.title}
         onChange={handleBackgroundChange}
       />
+      <label htmlFor="title">Institution</label>
       <input
         type="text"
         name="institution"
         value={background.institution}
         onChange={handleBackgroundChange}
       />
+      <label htmlFor="date1">Start Year</label>
       <input
-        type="date"
-        name="date"
-        value={background.date}
+        type="number"
+        name="date1"
+        value={background.date1}
+        onChange={handleBackgroundChange}
+      />
+      <label htmlFor="date2">End Year</label>
+      <input
+        type="number"
+        name="date2"
+        value={background.date2}
         onChange={handleBackgroundChange}
       />
     </form>
--- cv-application/src/components/JobExperience.jsx
+++ cv-application/src/components/JobExperience.jsx
@@ -1,36 +1,42 @@
-function jobExperience({experience ,handleExperienceChange}) {
+function jobExperience({ experience, handleExperienceChange }) {
   return (
     <form>
+      <label htmlFor="role">Job Title</label>
       <input
         type="text"
         name="role"
         value={experience.role}
         onChange={handleExperienceChange}
       />
+      <label htmlFor="company">Company</label>
       <input
         type="text"
         name="company"
         value={experience.company}
         onChange={handleExperienceChange}
       />
+      <label htmlFor="date1">Start Year</label>
       <input
-        type="date"
-        name="date"
-        value={experience.date}
+        type="number"
+        name="date1"
+        value={experience.date1}
         onChange={handleExperienceChange}
       />
+      <label htmlFor="date2">End Year</label>
+      <input
+        type="number"
+        name="date2"
+        value={experience.date2}
+        onChange={handleExperienceChange}
+      />
+      <label htmlFor="city">City</label>
       <input
         type="text"
         name="city"
         value={experience.city}
         onChange={handleExperienceChange}
       />
-      <input
-        type="tel"
-        name="phone"
-        value={experience.phone}
-        onChange={handleExperienceChange}
-      />
+      <label htmlFor="accomplishments">Description</label>
       <input
         type="text"
         name="accomplishments"
